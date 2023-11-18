3a5ba28885095ef8916791358ac4dc544895b335
Siow Hao Zhen <96342348+HaoZhenSiow@users.noreply.github.com>
2023-11-18 08:11:45 +0800

    improve code for header/dbl-img-marquee

--- src/components/hero-section/double-img-marquee.tsx
+++ src/components/hero-section/double-img-marquee.tsx
@@ -1,58 +1,123 @@
 import { component$, useStylesScoped$ } from '@builder.io/qwik'
+import styled from '~/lib/styled'
 import fluid from '~/lib/fluid'
+import useViewportSize from '~/hooks/useViewportSize'
 
 export default component$(() => {
   useStylesScoped$(createStyle())
 
+  const viewport = useViewportSize(),
+        isMidScreen = viewport.width >= 650
+
   return (
     <header>
       <div class="hero__visual container">
-        <div class="hero__visual__marquee">
-          <div class="imgList">
-            <div class="img"></div>
-            <div class="img"></div>
-            <div class="img"></div>
-            <div class="img"></div>
-          </div>
-          <div class="imgList">
-            <div class="img"></div>
-            <div class="img"></div>
-            <div class="img"></div>
-            <div class="img"></div>
-          </div>
-        </div>
-        <div class="hero__visual__marquee hero__visual__marquee--reverse">
-          <div class="imgList">
-            <div class="img"></div>
-            <div class="img"></div>
-            <div class="img"></div>
-            <div class="img"></div>
-          </div>
-          <div class="imgList">
-            <div class="img"></div>
-            <div class="img"></div>
-            <div class="img"></div>
-            <div class="img"></div>
-          </div>
-        </div>
+        <Marquee />
+        <Marquee reverse />
       </div>
       <div class="hero__content container">
-        <h1>Redefining Renovation Experience <br/>Where<br/> Trust Comes First</h1>
-        <p>
+        <h1 class="hero__title">Redefining Renovation Experience {isMidScreen && <br />}Where{!isMidScreen && <br />} Trust Comes First</h1>
+        <p class="hero__desc">
           The most reliable interior design firm in Singapore, backed by over X happy customers, where your dream home is delivered, not nightmares!
         </p>
         <button type="button" class="hero__cta">Get my Quote Now</button>
-        <small>It's free, no consultation fee</small>
+        <small class="hero__small">It's free, no consultation fee</small>
       </div>
     </header>
   )
 })
 
+const Marquee = component$(({ reverse }: {
+  reverse?: boolean
+}) => {
+  const heroImages = ['1', '2', '3', '4', '5']
+  const repeat = 2
+
+  useStylesScoped$(styled(`
+    .marquee {
+      --img-width: ${fluid(250, 300, 320, 1440)};
+      --ani-duration: 20s;
+      display: flex;
+      overflow: hidden;
+      position: relative;
+    }
+
+    .marquee:before, .marquee:after {
+      content: "";
+      position: absolute;
+      z-index: 1;
+      bottom: 0;
+      width: calc(var(--img-width) * 0.2);
+      height: 100%;
+      background-image: linear-gradient(to var(--dir), rgba(255,255,255,0), white 90%);
+    }
+
+    .marquee:before {
+      --dir: left;
+      left: -5px;
+    }
+
+    .marquee:after {
+      --dir: right;
+      right: -2px;
+    }
+
+    .marquee_row {
+      display: flex;
+      animation: marquee-move var(--ani-duration) linear infinite;
+    }
+
+    .marquee--reverse .marquee_row {
+      animation-direction: reverse;
+    }
+
+    .marquee__img {
+      aspect-ratio: 16/9;
+      width: var(--img-width);
+      background-color: grey;
+      margin-inline: calc(var(--img-width) * .04);
+      border-radius: 10px;
+      flex-shrink: 0;
+    }
+
+    @keyframes marquee-move {
+      from {
+        transform: translateX(0);
+      }
+      to {
+        transform: translateX(-100%);
+      }
+    }
+  `))
+  
+  return (
+    <div class={`marquee ${reverse && 'marquee--reverse'}`}>
+      {Array(repeat).fill('x').map((x, idx) => (
+        <div key={idx} class="marquee_row">
+          {heroImages.map((img, idx) => (
+            <div key={idx} class="marquee__img"></div>
+          ))}
+        </div>
+      ))}
+    </div>
+  )
+})
+
 function createStyle() {
-  return `
+  return styled(`
     header {
+      --title-fs: ${fluid(28, 48, 320, 1440)};
+      --cta-fs: 1em;
+      --cta-color: white;
+      --cta-bg: black;
       padding-top: ${fluid(100, 140, 320, 1440)};
       padding-bottom: 100px;
+    }
+
+    .hero__visual {
+      display: flex;
+      flex-direction: column;
+      gap: 20px;
     }
 
     .hero__content {
@@ -62,98 +127,23 @@ function createStyle() {
       text-align: center;
     }
 
-    .hero__content h1 {
-      font-size: ${fluid(28, 48, 320, 1440)};
+    .hero__title {
+      font-size: var(--title-fs);
       margin-top: 1.5em;
       margin-bottom: .5em;
     }
 
-    .hero__content h1 br:first-of-type {
-      display: none;
-    }
-
-    .hero__content p {
+    .hero__desc {
       max-width: 38.125em;
-    }
-
-    @media (700px <= width) {
-      .hero__content h1 br:first-of-type {
-        display: inline;
-      }
-
-      .hero__content h1 br:last-of-type {
-        display: none;
-      }
-    }
-
-    .hero__visual__marquee {
-      --img-width: ${fluid(250, 300, 320, 1440)};
-      display: flex;
-      overflow: hidden;
-      position: relative;
-    }
-
-    .hero__visual__marquee:before {
-      content: "";
-      position: absolute;
-      z-index: 1;
-      bottom: 0;
-      left: -5px;
-      pointer-events: none;
-      background-image: linear-gradient(to left, rgba(255,255,255,0), white 90%);
-      width: calc(var(--img-width) * 0.2);
-      height: 100%;
-    }
-
-    .hero__visual__marquee:after {
-      content: "";
-      position: absolute;
-      z-index: 1;
-      bottom: 0;
-      right: -2px;
-      pointer-events: none;
-      background-image: linear-gradient(to right, rgba(255,255,255,0), white 90%);
-      width: calc(var(--img-width) * 0.2);
-      height: 100%;
-    }
-
-    .hero__visual__marquee--reverse {
-      margin-top: 20px;
-    }
-
-    div.imgList {
-      display: flex;
-      animation: marquee-move-left 20s linear infinite;
-    }
-
-    .hero__visual__marquee--reverse div.imgList {
-      animation-direction: reverse;
-    }
-
-    div.img {
-      aspect-ratio: 16/9;
-      width: var(--img-width);
-      background-color: grey;
-      margin-inline: calc(var(--img-width) * .04);
-      flex-shrink: 0;
-    }
-
-    @keyframes marquee-move-left {
-      from {
-        transform: translateX(0);
-      }
-      to {
-        transform: translateX(-100%);
-      }
     }
 
     .hero__cta {
       font-weight: bold;
-      font-size: 1em;
+      font-size: var(--cta-fs);
       padding: 1em 2em;
-      color: white;
-      background-color: black;
-      margin-top: 2em;
+      color: var(--cta-color);
+      background-color: var(--cta-bg);
+      margin-top: 2.5em;
     }
-  `
+  `)
 }
