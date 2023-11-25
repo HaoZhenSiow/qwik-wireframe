ff9e9398d87753676e4c2a974b8c4c9216c64e48
Siow Hao Zhen <96342348+HaoZhenSiow@users.noreply.github.com>
2023-11-25 22:38:44 +0800

    disable font-face

--- src/root.tsx
+++ src/root.tsx
@@ -6,7 +6,7 @@ import {
 } from "@builder.io/qwik-city";
 import { RouterHead } from "./components/router-head/router-head";
 
-import './fonts.css'
+// import './fonts.css'
 import './init.css'
 import './global.css'
 
